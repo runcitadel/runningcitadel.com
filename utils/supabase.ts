8f2889c001507ccd7edadda0b5e01d9b13d7ce23
AaronDewes <aaron.dewes@protonmail.com>
2023-01-06 20:22:04 +0000

    Fix JS

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,12 +1,14 @@
 import { installGlobals } from "https://deno.land/x/virtualstorage@0.1.0/mod.ts";
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js?dts";
+import { createClient, SupabaseClient } from "https://cdn.skypack.dev/@supabase/supabase-js?dts";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Database } from "./db.ts";
 
-installGlobals();
-
+let client: SupabaseClient<Database> | null = null;
 if (IS_BROWSER) {
-    throw new Error("This module is not intended for use in the browser.");
+    console.warn("Supabase module imported in browser");
+} else {
+    installGlobals();
+    client = createClient<Database>(Deno.env.get("SUPABASE_URL")!, Deno.env.get("SUPABASE_ADMIN_KEY")!);
+    
 }
-const client = createClient<Database>(Deno.env.get("SUPABASE_URL")!, Deno.env.get("SUPABASE_ADMIN_KEY")!);
-export default client;
+export default client as SupabaseClient<Database>;
