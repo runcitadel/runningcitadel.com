1fc63b762eb699ac5c4d2fb1a318d93ae402933f
Aaron Dewes <aaron.dewes@protonmail.com>
2023-02-06 17:53:44 +0100

    Update _middleware.ts

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,13 +1,15 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
-
-export async function handler(
+import { getLogger } from "https://deno.land/x/fresh_logging@1.1.2/index.ts";
+export const handler = [
+  getLogger(),
+  (
     req: Request,
     ctx: MiddlewareHandlerContext,
-  ) {
+  ) => {
     const resp = await ctx.next();
     if (req.url.startsWith("https://")) {
       resp.headers.set("Strict-Transport-Security", "max-age=94608000; includeSubDomains; preload");
     }
     return resp;
   }
-  
+];
